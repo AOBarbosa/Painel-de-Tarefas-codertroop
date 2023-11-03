0eaba7248454326be813af0c86d989aeb9a8dac7
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-11-03 00:07:55 -0300

    Att testes

--- src/__tests__/components/TaskArea.spec.tsx
+++ src/__tests__/components/TaskArea.spec.tsx
@@ -1,71 +1,98 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import { TaskArea } from '@/components/TaskArea'
 
-describe('Task Page', () => {
-  it('should be able to add a new task', () => {
-    render(<TaskArea />)
+describe('Task', () => {
+  describe('add tasks', () => {
+    it('should be able to add a new task', () => {
+      render(<TaskArea />)
 
-    const taskInput = screen.getByPlaceholderText('Insira uma nova tarefa')
-    const addTaskButton = screen.getByTestId('add-task-button')
+      const taskInput = screen.getByPlaceholderText('Insira uma nova tarefa')
+      const addTaskButton = screen.getByTestId('add-task-button')
 
-    fireEvent.change(taskInput, {
-      target: {
-        value: 'Nova tarefa',
-      },
+      fireEvent.change(taskInput, {
+        target: {
+          value: 'Nova tarefa',
+        },
+      })
+      fireEvent.click(addTaskButton)
+
+      const newTaskTitle = screen.getByText('Nova tarefa')
+
+      expect(newTaskTitle).toHaveTextContent('Nova tarefa')
+      expect(newTaskTitle.parentElement).not.toHaveClass('line-through')
+
+      fireEvent.change(taskInput, {
+        target: {
+          value: 'Ler livro',
+        },
+      })
+      fireEvent.click(addTaskButton)
+
+      const addedSecondTaskTitle = screen.getByText('Ler livro')
+
+      expect(newTaskTitle).toBeInTheDocument()
+      expect(newTaskTitle).toHaveTextContent('Nova tarefa')
+      expect(newTaskTitle.parentElement).not.toHaveClass('line-through')
+
+      expect(addedSecondTaskTitle).toHaveTextContent('Ler livro')
+      expect(addedSecondTaskTitle.parentElement).not.toHaveClass('line-through')
     })
-    fireEvent.click(addTaskButton)
 
-    const newTaskTitle = screen.getByText('Nova tarefa')
+    it('should not be able to add an empty task', () => {
+      render(<TaskArea />)
 
-    expect(newTaskTitle).toHaveTextContent('Nova tarefa')
-    expect(newTaskTitle.parentElement).not.toHaveClass('line-through')
+      const addTaskButton = screen.getByTestId('add-task-button')
+      fireEvent.click(addTaskButton)
 
-    fireEvent.change(taskInput, {
-      target: {
-        value: 'Ler livro',
-      },
+      expect(screen.queryByTestId('task')).not.toBeInTheDocument()
     })
-    fireEvent.click(addTaskButton)
-
-    const addedSecondTaskTitle = screen.getByText('Ler livro')
-
-    expect(newTaskTitle).toBeInTheDocument()
-    expect(newTaskTitle).toHaveTextContent('Nova tarefa')
-    expect(newTaskTitle.parentElement).not.toHaveClass('line-through')
-
-    expect(addedSecondTaskTitle).toHaveTextContent('Ler livro')
-    expect(addedSecondTaskTitle.parentElement).not.toHaveClass('line-through')
   })
 
-  it('should not be able to add an empty task', () => {
-    render(<TaskArea />)
+  describe('remove tasks', () => {
+    it('should be able to remove a task', () => {
+      render(<TaskArea />)
 
-    const addTaskButton = screen.getByTestId('add-task-button')
-    fireEvent.click(addTaskButton)
+      const taskInput = screen.getByPlaceholderText('Insira uma nova tarefa')
+      const addTaskButton = screen.getByTestId('add-task-button')
 
-    expect(screen.queryByTestId('task')).not.toBeInTheDocument()
-  })
+      fireEvent.change(taskInput, {
+        target: {
+          value: 'Nova tarefa',
+        },
+      })
+      fireEvent.click(addTaskButton)
 
-  it('should be able to remove a task', () => {
-    render(<TaskArea />)
+      const newTaskTitle = screen.getByText('Nova tarefa')
+      expect(newTaskTitle).toBeInTheDocument()
 
-    const taskInput = screen.getByPlaceholderText('Insira uma nova tarefa')
-    const addTaskButton = screen.getByTestId('add-task-button')
+      const removeTaskButton = screen.getByTestId('remove-task')
+      fireEvent.click(removeTaskButton)
 
-    fireEvent.change(taskInput, {
-      target: {
-        value: 'Nova tarefa',
-      },
+      expect(newTaskTitle).not.toBeInTheDocument()
     })
-    fireEvent.click(addTaskButton)
 
-    const newTaskTitle = screen.getByText('Nova tarefa')
-    expect(newTaskTitle).toBeInTheDocument()
+    it('should not be able to remove a checked task', () => {
+      render(<TaskArea />)
 
-    const removeTaskButton = screen.getByTestId('remove-task')
-    fireEvent.click(removeTaskButton)
+      const taskInput = screen.getByPlaceholderText('Insira uma nova tarefa')
+      const addTaskButton = screen.getByTestId('add-task-button')
 
-    expect(newTaskTitle).not.toBeInTheDocument()
+      fireEvent.change(taskInput, {
+        target: {
+          value: 'Nova tarefa',
+        },
+      })
+      fireEvent.click(addTaskButton)
+
+      const task = screen.getByText('Nova tarefa')
+      task.firstChild && fireEvent.click(task.firstChild)
+
+      const removeTaskButton = screen.getByTestId('remove-task')
+
+      expect(task).toBeInTheDocument()
+      expect(task).toHaveClass('line-through')
+      expect(removeTaskButton).toBeDisabled()
+    })
   })
 
   it('should be able to check a task as completed', () => {
